7f23783bf2146b40107274f93a0a0f21ce9791c2
Maru <maru40137@gmail.com>
2022-05-28 08:12:31 +0900

    [Learn] Suppoer learning

--- src/job.rs
+++ src/job.rs
@@ -3,7 +3,7 @@ use crate::loader;
 use crate::sex::Sex;
 
 use enum_iterator::IntoEnumIterator;
-use enum_map::{Enum, EnumMap};
+use enum_map::{enum_map, Enum, EnumMap};
 use fixed::types::U4F4;
 use std::ops::RangeInclusive;
 
@@ -14,25 +14,25 @@ pub type GrowthValueT = U4F4;
 pub enum Job {
     #[strum(serialize = "せんし", serialize = "せ")]
     Soldier,
-    #[strum(serialize = "そうりょ", serialize = "そ")]
-    Pligrim,
-    #[strum(serialize = "まほうつかい", serialize = "ま")]
-    Wizard,
     #[strum(serialize = "ぶとうか", serialize = "ぶ")]
     Fighter,
+    #[strum(serialize = "まほうつかい", serialize = "ま")]
+    Wizard,
+    #[strum(serialize = "そうりょ", serialize = "そ")]
+    Pligrim,
     #[strum(serialize = "しょうにん", serialize = "し")]
     Merchant,
     #[strum(serialize = "あそびにん", serialize = "あ")]
     GoofOff,
     #[strum(serialize = "とうぞく", serialize = "と")]
     Thief,
-    #[strum(serialize = "ゆうしゃ", serialize = "ゆ")]
-    Hero,
     #[strum(serialize = "けんじゃ", serialize = "け")]
     Sage,
+    #[strum(serialize = "ゆうしゃ", serialize = "ゆ")]
+    Hero,
 }
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct Growth {
     upper_lv: u8,
     value: GrowthValueT,
@@ -43,11 +43,13 @@ pub struct JobTable {
     name: String,
     exps: [u32; 98],
     attr_inits: EnumMap<Attr, EnumMap<Sex, u8>>,
-    attr_growths: EnumMap<Attr, [Growth; 5]>,
+    attr_growths: EnumMap<Attr, Vec<Growth>>,
 }
 
 impl JobTable {
     pub fn growth_value(&self, lv: u8, attr: Attr) -> GrowthValueT {
+        assert!(lv >= 2);
+
         let growths = &self.attr_growths[attr];
 
         // Search growth step id.
@@ -62,15 +64,15 @@ impl JobTable {
         growths[id].value
     }
 
-    pub fn standard_attr_value(&self, lv: u8, attr: Attr) -> u8 {
-        let init = self.attr_inits[attr][Sex::Man];
+    fn sum_growth_value(&self, lv: u8, attr: Attr) -> u8 {
+        assert!(lv >= 1);
 
         if lv == 1 {
-            return init;
+            return 0;
         }
 
         let sum = (2..=lv)
-            .fold(AttrValue::from(init), |acc, lv| {
+            .fold(AttrValue::from(0), |acc, lv| {
                 acc.saturating_add(AttrValue::from(self.growth_value(lv, attr)))
             })
             .to_num();
@@ -78,7 +80,17 @@ impl JobTable {
         sum
     }
 
+    pub fn standard_attr_value(&self, lv: u8, attr: Attr) -> u8 {
+        assert!(lv >= 1);
+
+        let init = self.attr_inits[attr][Sex::Man];
+
+        init + self.sum_growth_value(lv, attr)
+    }
+
     pub fn range_attr_value(&self, lv: u8, attr: Attr) -> RangeInclusive<u8> {
+        assert!(lv >= 1);
+
         let standard = self.standard_attr_value(lv, attr);
         let upper = standard.saturating_add(15).saturating_add(lv * 2);
 
@@ -92,6 +104,20 @@ impl JobTable {
 
         lower..=upper
     }
+
+    pub fn standard_intelligence_by_learning(&self, lv: u8) -> u8 {
+        assert!(lv >= 2);
+
+        self.sum_growth_value(lv - 1, Attr::Int) + 5
+    }
+
+    pub fn intelligence_thresh_for_learning(&self, lv: u8) -> (u8, u8) {
+        assert!(lv >= 2);
+
+        let std = self.standard_intelligence_by_learning(lv);
+
+        (std.saturating_sub(15), std.saturating_add(11))
+    }
 }
 
 impl loader::FromRecord for JobTable {
@@ -103,25 +129,29 @@ impl loader::FromRecord for JobTable {
         }
 
         let mut attr_inits = EnumMap::<Attr, EnumMap<Sex, u8>>::default();
-        let mut attr_growths = EnumMap::<Attr, [Growth; 5]>::default();
+        let mut attr_growths = EnumMap::<Attr, Vec<Growth>>::default();
 
-        for (status_i, status) in Attr::into_enum_iter().enumerate() {
+        let attr_growth_nums = [5, 5, 6, 5, 5];
+
+        for (attr_i, attr) in Attr::into_enum_iter().enumerate() {
+            let growth_num_sum = attr_growth_nums[0..attr_i].iter().sum::<usize>();
+            let offset: usize = 2 * attr_i + 2 * growth_num_sum;
+
             // Parse initial status.
             let mut inits = EnumMap::<Sex, u8>::default();
             for (sex_i, sex) in Sex::into_enum_iter().enumerate() {
-                inits[sex] = record[110 + 12 * status_i + sex_i].parse().unwrap();
+                inits[sex] = record[110 + offset + sex_i].parse().unwrap();
             }
-            attr_inits[status] = inits;
+            attr_inits[attr] = inits;
 
             // Parse growth of status.
-            let mut growths: [Growth; 5] = Default::default();
+            let mut growths = vec![Growth::default(); attr_growth_nums[attr_i]];
             for (growth_i, growth) in growths.iter_mut().enumerate() {
-                growth.upper_lv = record[112 + 12 * status_i + 2 * growth_i].parse().unwrap();
-                growth.value = GrowthValueT::from_bits(
-                    record[113 + 12 * status_i + 2 * growth_i].parse().unwrap(),
-                );
+                growth.upper_lv = record[112 + offset + 2 * growth_i].parse().unwrap();
+                growth.value =
+                    GrowthValueT::from_bits(record[113 + offset + 2 * growth_i].parse().unwrap());
             }
-            attr_growths[status] = growths;
+            attr_growths[attr] = growths;
         }
 
         Self {
@@ -151,5 +181,6 @@ mod tests {
     #[test]
     fn test_growth_value() {
         assert_eq!(get_job_table(Job::Soldier).growth_value(9, Attr::Vit), 5.5);
+        assert_eq!(get_job_table(Job::Sage).growth_value(2, Attr::Int), 0.5);
     }
 }
